8d6e3d7189addcf284e9c7584f091ee3efbea757
Giacomo <giacomo.boschele@marconirovereto.it>
2022-09-28 16:23:55 +0200

    added user-service added some java files

---
+++ userservice/src/main/java/com/auth/domain/UserDetails.java
@@ -0,0 +1,5 @@
+package main.java.com.auth.domain;
+
+public class UserDetails {
+    
+}
---
+++ userservice/src/main/java/com/auth/service/AuthService.java
@@ -0,0 +1,9 @@
+package main.java.com.auth.service;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+    /* */
+    public void Register(){}
+    public void Login(){}
+}
